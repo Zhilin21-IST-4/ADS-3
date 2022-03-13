d945ca50e5eec8c3493d97040cd39dcc1a9e7fd7
Zhilin21-IST-4 <94217280+Zhilin21-IST-4@users.noreply.github.com>
2022-03-13 16:41:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,11 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
+  int k = 0;
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == value) {
+      k++;
+    }
+  }
   return 0; // если ничего не найдено
 }
