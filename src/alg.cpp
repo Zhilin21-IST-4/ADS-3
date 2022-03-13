5af04a650c2010003d0e428922fde9e95d047b5e
Zhilin21-IST-4 <94217280+Zhilin21-IST-4@users.noreply.github.com>
2022-03-13 17:50:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,24 +3,11 @@
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
   int k = 0;
-  int i = 0; 
-  int j = size - 1;
-  while (i < j) {
-    int middle = i + (i - j) / 2;
-    if (arr[middle] == value) {
-      int mid1 = middle;
-      while (arr[--middle] == value)
-        k++;
-      while (arr[++mid1] == value)
-        k++;
-      return k;
-    }
-    else if (arr[middle] > value) {
-      j = middle;
-    }
-    else {
-      i = middle + 1;
+  for (int i = 0; i < size; i++) {
+    if (value == arr[i]) {
+      k++;
     }
   }
+  return k;
   return 0; // если ничего не найдено
 }
